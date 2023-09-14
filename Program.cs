a7730240a85adb9e80cbad7f4fa4c387e8083476
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-14 04:15:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Return_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("the cube of the number you entered is ");
+            Console.WriteLine(Cube(userInput));
+
+            Console.ReadLine();
+
+        }
+    static int Cube(int x)
+        {
+            return x * x * x;
+
+        }
+
+
+
+    }
+
+}
